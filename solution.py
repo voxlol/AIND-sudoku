85b4ae25717dd6bf5bad1ef18c150428a9f21a7a
voxlol <voxlol@outlook.com>
2017-02-06 00:28:17 -0600

    Complete reduce_puzzle function

--- solution.py
+++ solution.py
@@ -81,7 +81,17 @@ def only_choice(values):
     return new_values
 
 def reduce_puzzle(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for boxi n values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
     pass
